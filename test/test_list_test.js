91191ed0298487676e15f84815e9491e56e8b74e
Taka Okunishi <okunishinishi@gmail.com>
2017-06-21 10:53:16 +0900

    Add test for $ge/$le

--- test/test_list_test.js
+++ test/test_list_test.js
@@ -6,22 +6,41 @@
 
 const testList = require('../lib/cases/test_list.js')
 const clayDriverMemory = require('clay-driver-memory')
+const clayDriverSequelize = require('clay-driver-sequelize')
 const assert = require('assert')
 const co = require('co')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const rimraf = require('rimraf')
 
 describe('test-list', function () {
   this.timeout(3000)
 
-  before(() => co(function * () {
+  let storage01 = `${__dirname}/../tmp/testing-list-driver.db`
 
+  before(() => co(function * () {
+    let storages = [ storage01 ]
+    for (let storage of storages) {
+      rimraf.sync(storage)
+      mkdirp.sync(path.dirname(storage))
+    }
   }))
 
   after(() => co(function * () {
 
   }))
 
-  it('Test list', () => co(function * () {
+  it('Test list with memory', () => co(function * () {
     let driver = clayDriverMemory({})
+    yield testList(driver)
+  }))
+  it('Test list with sqlite', () => co(function * () {
+    let driver = clayDriverSequelize('hoge', '', '', {
+      storage: storage01,
+      dialect: 'sqlite',
+      benchmark: true,
+      logging: console.log
+    })
     yield testList(driver)
   }))
 })
